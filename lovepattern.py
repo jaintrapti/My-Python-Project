61b6a511d4a21b3000f9ef0f751c520ee2fb4cfe
Pari Jain <parij16@gmail.com>
2020-08-10 17:05:52 +0530

    first time commit

---
+++ lovepattern.py
@@ -0,0 +1,46 @@
+str1 = "LOVE"
+print__L = [[" " for i in range(5)] for j in range(5)]
+print__O = [[" " for i in range(5)] for j in range(5)]
+print__V = [[" " for i in range(5)] for j in range(5)]
+print__E = [[" " for i in range(5)] for j in range(5)]
+
+
+#code for L
+for i in range(5):
+    for j in range(5):
+        if j==0 or (i==5 and j>0):
+            print__L[i][j]= "*"
+
+#code for O          
+for i in range(5):
+    for j in range(5):
+        if((j==0 or j==5) and (i!=0 and i!=5)) or ((i==0 or i==5) and (j>0 or j<5)):
+            print__O[i][j]= "*"
+
+
+#code for V           
+for i in range(5):
+    for j in range(5):
+        if ((i==0 and j==0) or (i==2 and j==1) or (i==4 and j==2) or (i==2 and j==3) or (i==0 and j==4)):
+            print__V[i][j]= "*"
+
+#code for E              
+for i in range(5):
+    for i in range(5):
+        if j==0 or ((i==0 or i==3 or i==5) and j>0):
+            print__E[i][j]= "*"
+              
+for i in range(5):
+    for j in range(5):
+        print(print__L[i][j],end=" ")
+    print(end="  ")
+    for j in range(5):
+        print(print__O[i][j],end=" ")
+    print(end="  ")
+    for j in range(5):
+        print(print__V[i][j],end=" ")
+    print(end="  ")
+    for j in range(5):
+        print(print__E[i][j],end=" ")
+    print()
+    
